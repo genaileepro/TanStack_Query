0980f64e214596e73d2801aa6540ea69affc6412
Daese <daeseonglee7@gmail.com>
2024-07-15 10:00:39 +0900

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import TodoListContainer from "./components/TodoListContainer";
-import AddForm from "./components/AddForm";
-import { fetchTodos } from "./redux/slices/todos"; // 비동기 액션 가져오기
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    // 컴포넌트 마운트 시 todos를 가져오는 비동기 액션 디스패치
-    dispatch(fetchTodos());
-  }, [dispatch]);
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <AddForm />
-      <TodoListContainer />
-    </div>
-  );
-};
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
 
-export default App;
+export default App
